06d063befded3a117b210705d9985a2c27ac9dd5
Derick Mota <Derick Mota@DESKTOP-1KAL186>
2026-04-18 20:11:30 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/javaproyect2/JavaProyect2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaproyect2;
+
+/**
+ *
+ * @author umg
+ */
+public class JavaProyect2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
